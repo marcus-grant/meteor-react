329f39573a647fa237535fc5b49c6773f9dec615
Marcus Grant <marcusfg@gmail.com>
2017-09-20 18:18:25 -0400

    S3L16, math & utils no longer needed

--- score-keep/imports/utils.js
+++
@@ -1,9 +0,0 @@
-console.log('Log from /imports/utils.js');
-
-export let greetUser = function () {
-  return 'Hello Poop-face!';
-};
-
-export let name = 'Mr. Poopy-Butthole';
-
-export default 'Default val';
