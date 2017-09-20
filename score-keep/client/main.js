5019c2ee13b293ea54cd01f8dd88b2e257ffb6e1
Marcus Grant <marcusfg@gmail.com>
2017-09-20 18:15:29 -0400

    up to s3/lesson14, rendering static data

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -2,8 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 
+const players = [{
+  _id: '1',
+  name: 'Lauren',
+  score: 99
+}, {
+  _id: '2',
+  name: 'Cory',
+  score: -1
+}, {
+  _id: '3',
+  name: 'Andrew',
+  score: -12
+}];
+
+const renderPlayers= function (playerList) {
+  return playerList.map(function (player) {
+    return <p key={player._id}>{player.name} has {player.score} points!</p>;
+  });
+};
+
 Meteor.startup(function () {
     // Render JSX stuff here...
-    let jsx = <p>wuba-luba-dub-dub</p>;
+    let name = 'Mike';
+    let title = 'Account Settings';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is my second p.</p>
+        {renderPlayers(players)}
+      </div>
+    )
     ReactDOM.render(jsx, document.getElementById('react-root'));
 });
