e0d5e4a653f305378c92ecf7e8f105ed25b48a49
Marcus Grant <marcusfg@gmail.com>
2017-09-21 18:57:18 -0400

    remove static additions to database

--- score-keep/server/main.js
+++ score-keep/server/main.js
@@ -2,9 +2,5 @@ import {Meteor} from 'meteor/meteor'
 import {Players} from './../imports/api/players';
 
 Meteor.startup( () => {
-  //  Players.insert({
-  //    name: 'Andrew',
-  //    score: 3
-  //  });
-  //  console.log(Players.find());
+  
 });
