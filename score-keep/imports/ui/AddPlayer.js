6b26ef2fbd7760489df36c9cf878db7db4ec787d
Marcus Grant <marcusfg@gmail.com>
2017-09-21 18:57:40 -0400

    first seperated react components

---
+++ score-keep/imports/ui/AddPlayer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class AddPlayer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>AddPlayer</h3>
+      </div>
+    );
+  }
+}
+
+/*<form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player Name"/>
+          <button>Add Player</button
+        </form> */
---
+++ score-keep/imports/ui/TitleBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class TitleBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+      </div>
+    );
+  }
+}
+
+TitleBar.propTypes = {
+  title: React.PropTypes.string
+};
+
+TitleBar.defaultProps = {
+  title: 'Default Title'
+};
