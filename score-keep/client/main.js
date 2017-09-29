bde8630d632a567a832d458e5610239157e753ab
Marcus Grant <marcus.grant@patternbuffer.io>
2017-09-28 22:04:18 -0400

    rebase from udemy course due to missed push - Forgot to push on 2017-09-28 - Need to rebase to keep working on it - Essentially, the same as when course reaches section 5 lect. 41 - Will push to master which can then be pulled to s5/react-style

--- score-keep/client/main.js
+++ score-keep/client/main.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
-import {Players} from './../imports/api/players';
 import {Tracker} from 'meteor/tracker';
-import App from '../imports/ui/App.jsx';
+
+import {Players} from './../imports/api/players';
+import App from './../imports/ui/App';
 
 Meteor.startup(() => {
-  // Create a players list 'players' by fetching from datastore
-  players = Players.find().fetch();
   Tracker.autorun(() => {
-    // Update players on datastore updates
     let players = Players.find({}, {sort: {score: -1}}).fetch();
     let title = 'Score Keep';
-
-    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('react-root'));
-  })
+    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
+  });
 });
--- score-keep/imports/ui/PlayerList.js
+++ score-keep/imports/ui/PlayerList.js
@@ -3,27 +3,24 @@ import React from 'react';
 import Player from './Player';
 
 export default class PlayerList extends React.Component {
-  renderPlayers= (playerList) => {
+  renderPlayers() {
     if (this.props.players.length === 0) {
-      // return some jsx if empty
-      return (
-        <div>
-          <h3>No Score to track...</h3>
-          <p>Please add some players to track their scores.</p>
-        </div>
-      );
+      return <p>Add your first player to get started!</p>;
     } else {
-    return this.props.players.map((player) => {
-     return <Player key={player._id} player={player} />;
-    });
+      return this.props.players.map((player) => {
+        return <Player key={player._id} player={player}/>;
+      });
     }
   }
-
   render() {
     return (
       <div>
-        {this.renderPlayers()} 
+        {this.renderPlayers()}
       </div>
     );
   }
+};
+
+PlayerList.propTypes = {
+  players: React.PropTypes.array.isRequired
 }
