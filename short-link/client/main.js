d9ad8de86cfa663e2a1ea84797b9db7914c54964
Marcus Grant <marcus.grant@patternbuffer.io>
2017-10-01 03:09:05 -0400

    main.js now launches react app with Routed Routes

--- short-link/client/main.js
+++ short-link/client/main.js
@@ -1,22 +1,23 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
-import './main.html';
+import Signup from '../imports/ui/Signup';
+import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound.js';
+import Login from '../imports/ui/Login.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Login} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/links" component={Link} />
+    <Route path="*" component={NotFound} />
+  </Router>
+);
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+Meteor.startup(() => {
+  ReactDOM.render(routes, document.getElementById('app'));
 });
